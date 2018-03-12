7b1a16f2b557ea80c617a3836dee6a20934869d5
zhukoff-av <mr.zhukoff@gmail.com>
2018-03-12 23:06:31 +0300

    add gendiff

---
+++ src/bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import program from 'commander';
+
+program
+  .version('0.0.1')
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
