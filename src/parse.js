935715f1cfb57879ee5afeeb95b2d179b1c49995
anzhukov <anton.zhukov@t-systems.ru>
2018-03-15 18:28:06 +0300

    add ini parsing

--- src/parse.js
+++ src/parse.js
@@ -1,8 +1,10 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parsing = {
   '.json': JSON.parse,
   '.yml': yaml.safeLoad,
+  '.ini': ini.parse,
 };
 
 export default format => (someData) => {
