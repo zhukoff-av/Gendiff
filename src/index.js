2f9abac11f605b93b8259e3c6187dee38a3d758a
zhukoff-av <mr.zhukoff@gmail.com>
2018-03-17 02:42:19 +0300

    change import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import path from 'path';
 import fs from 'fs';
-
 import yaml from 'js-yaml';
 import ini from 'ini';
-
-import makeAst from './ast';
+import doAST from './ast';
 import renders from './renders';
 
 const readFile = pathToFile => fs.readFileSync(path.normalize(pathToFile), 'utf8');
@@ -19,7 +17,7 @@ const getContent = (...paths) => paths
   .map(exactPath => parser[path.extname(exactPath)](readFile(exactPath)));
 
 const gendiff = (pathToFile1, pathToFile2, render = renders.default) => {
-  const diffAst = makeAst(...getContent(pathToFile1, pathToFile2));
+  const diffAst = doAST(...getContent(pathToFile1, pathToFile2));
 
   return render(diffAst);
 };
